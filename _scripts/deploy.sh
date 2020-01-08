00873c42681cfa2cef1137bf1f3f152510d6344b
caminek <steven.warren@gmail.com>
2020-01-08 14:11:42 -0500

    Updated git remote

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -11,7 +11,15 @@ mv _site/ docs/
 git config user.email ${GITHUB_EMAIL}
 git config user.name "caminek-travis"
 
+
+
+git@github.com:caminek/caminek.github.io.git
+
+
+
 # Push
+git remote rm origin
+git remote add origin https://TravisCI_Token:$GITHUB_TOKEN@github.com:caminek/caminek.github.io.git
 git add .
 git status
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
