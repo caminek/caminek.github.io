30c991fa394364156b660a7b61110f65a669b635
caminek <steven.warren@gmail.com>
2020-01-08 13:51:09 -0500

    Created deploy script

---
+++ _scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# halt script on error
+set -e 
+
+# Rename compiled site folder to docs for use with GH-Pages
+echo $PWD
+mv _site/ docs/
+
+# Credentials
+git config user.email ${GITHUB_EMAIL}
+git config user.name "caminek-travis"
+
+# Push
+git add .
+git status
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push origin master
